1adf216bd2fac77106c8c972e31b74fd83f6d4d6
Patrickcxt <xiongtaocxt@gmail.com>
2016-10-01 08:05:41 +0800

    fix DataHandler

--- DataHandler.lua
+++ DataHandler.lua
@@ -4,15 +4,17 @@ require 'image'
 
 local DataHandler = torch.class("DataHandler")
 
-function DataHandler:__init()
-    self.root = "/home/t5/cxt/VRAM/data/ActivityNet/"
+function DataHandler:__init(root)
+    self.root = root or "/home/t5/cxt/VRAM/data/ActivityNet/"
     self.indexPath = self.root .. "scheme/index.txt"
     self.gtPath = self.root .. "scheme/activity_net.v1-3.min.json"
     self.videoList, self.cutNameList = self:create_video_list()
     self.database, self.version, self.taxonomy = self:parse_json()
-    self.trainSet, self.valSet, self.testSet = self:create_dataset()
-    print(self.database["YEZrwxz0Ysk"])
     self.root = self:create_taxonomy_tree()
+    self.name2label, self.label2name = self:get_classes()
+    self.trainSet, self.valSet, self.testSet = self:create_dataset()
+    print(self.database)
+    --print(self.database)
 end
 
 function DataHandler:create_video_list()
@@ -53,17 +55,73 @@ function DataHandler:create_dataset()
 	elseif self.database[name]['subset'] == 'testing' then
 	    table.insert(testSet, self.videoList[i])
 	end
+
+        -- Get GT labels and segments
+	local annotations = self.database[name]["annotations"]
+	local duration = self.database[name]["duration"]
+	local gt_segments = torch.Tensor(#annotations, 2)
+	local gt_labels = torch.Tensor(#annotations)
+	for j = 1, #annotations do
+	    local segment = annotations[j]["segment"]
+	    local st, ed = segment[1] / duration, segment[2] / duration
+	    print(st, ed)
+	    io.read()
+	    gt_segments[j] = torch.Tensor({st, ed})
+	    local labelName = annotations[j]["label"]
+	    gt_labels[j] = self.name2label[labelName]
+	end
+	self.database[name]["gt_segments"] = gt_segments
+	self.database[name]["gt_labels"] = gt_labels
     end
     return trainSet, valSet, testSet
 
-    -- Refine Database
+end
 
-
+function DataHandler:get_classes()
+    local name2label = {}
+    local label2name = {}
+    local cnt = 0
+    for id, taxon in pairs(self.id2Tax) do
+        if #taxon["child"] == 0 then
+	    name2label[taxon["nodeName"]] = cnt
+	    label2name[cnt] = taxon["nodeName"]
+	    cnt = cnt + 1
+	end
+    end
+    return name2label, label2name
 end
 
 function DataHandler:create_taxonomy_tree()
-    -- Construct taxonomy tree
-    return nil
+    self.id2Tax = {}  -- nodeId -> taxonomy
+    self.subTree = {}  --> subtree rooted from nodeId
+    for i = 1, #self.taxonomy do
+        --local id = self.taxonomy[i]["nodeId"]
+	local nodeId = self.taxonomy[i]["nodeId"]
+	self.id2Tax[nodeId] = self.taxonomy[i]
+	local parentId = self.taxonomy[i]["parentId"]
+	if not self.subTree[parentId] then
+	    self.subTree[parentId] = {}
+	end
+	table.insert(self.subTree[parentId], nodeId)
+    end
+    local root = self:recursive_contruct_tree(0)
+    return root
+end
+
+function DataHandler:recursive_contruct_tree(rootid)
+    local child = {}
+    local annotation = self.id2Tax[rootid] -- note: id2Tax will be changed along with annotation( child added)
+    local subroot = self.subTree[rootid]
+    if not subroot then
+        self.subTree[rootid] = {}
+	annotation["child"] = child
+	return annotation
+    end
+    for _, cid in pairs(subroot) do
+        table.insert(child, self:recursive_contruct_tree(cid)) 
+    end
+    annotation["child"] = child
+    return annotation
 end
 
 function DataHandler:cut_name(name)
